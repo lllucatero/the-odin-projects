bbb9da1e6bbd0de35c51a18abc52561ebebf5440
Lucas Pontes <lcspontes@gmail.com>
2022-08-15 20:25:39 -0300

    First implemetation. Working, but needs refactor

--- sub_strings/sub_strings.rb
+++ sub_strings/sub_strings.rb
@@ -1,4 +1,21 @@
-def sub_strings(word, dictionary)
+dictionary = ["below","down","go","going","horn","how","howdy",
+                  "it", "i","low","own","part","partner","sit"]
 
+def substrings(string, dictionary)
 
+  matches = {}
+  dictionary.each do |word_dictionary|
+    string.downcase.split.each do |word_string|
+      if word_string.include? word_dictionary
+        if matches[word_dictionary].nil? == true
+          matches[word_dictionary] = 1
+        else
+          matches[word_dictionary] += 1
+        end
+      end
+    end
+  end
+  p matches
 end
+  
+substrings("Howdy partner, sit down! How's it going?", dictionary)
