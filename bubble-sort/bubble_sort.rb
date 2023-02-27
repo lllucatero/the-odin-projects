72cd5fcea8f0fc0252cfb81e896d7c76a4c935db
Lucas Pontes <lcspontes@gmail.com>
2023-02-27 14:31:15 -0300

    Apply changes according to the Ruby Style Guide

--- bubble-sort/bubble_sort.rb
+++ bubble-sort/bubble_sort.rb
@@ -1,15 +1,17 @@
+# frozen_string_literal: true
+
 def bubble_sort(array)
   loop do
     changed = false
     array.each_with_index do |_number, index|
-      if array[index+1] != nil && array[index] > array[index+1]
-        array[index], array[index+1] = array[index+1], array[index]
+      if !array[index + 1].nil? && array[index] > array[index + 1]
+        array[index], array[index + 1] = array[index + 1], array[index]
         changed = true
       end
     end
-    break if not changed
+    break unless changed
   end
   p array
 end
 
-bubble_sort([5,4,3,2,1])
+bubble_sort([5, 4, 3, 2, 1])
